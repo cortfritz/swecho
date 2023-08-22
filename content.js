7023b0f23d71f7df99b19a1b6fa534f1ea65539a
Cort Fritz <cort@rheli.co>
2023-08-22 15:33:00 -0700

    change strategy to hotkey copy-from

--- content.js
+++ content.js
@@ -1,66 +1,61 @@
-const isSwaggerPage = document.querySelector("body > div#swagger-ui");
+(function () {
+  const isSwaggerPage = document.querySelector("body > div#swagger-ui");
 
-if (!isSwaggerPage) {
-  // Exit the script if it's not a Swagger page
-  return;
-}
+  if (!isSwaggerPage) {
+    // Exit the script if it's not a Swagger page
+    return;
+  }
 
-document.addEventListener("input", function (event) {
-  // Fetch the 'swechoEnabled' value from Chrome storage
-  chrome.storage.local.get("swechoEnabled", function (data) {
-    if (
-      event.target.tagName === "INPUT" &&
-      event.target.closest("tr[data-param-name]") &&
-      data.swechoEnabled
-    ) {
-      const paramName = event.target
-        .closest("tr[data-param-name]")
-        .getAttribute("data-param-name");
-      const value = event.target.value;
+  document.addEventListener("input", function (event) {
+    // Fetch the 'swechoEnabled' value from Chrome storage
+    chrome.storage.local.get("swechoEnabled", function (data) {
+      if (
+        event.target.tagName === "INPUT" &&
+        event.target.closest("tr[data-param-name]") &&
+        data.swechoEnabled
+      ) {
+        const paramName = event.target
+          .closest("tr[data-param-name]")
+          .getAttribute("data-param-name");
+        const value = event.target.value;
 
-      // Find all input fields with the same parameter name and update them
-      const matchingInputs = document.querySelectorAll(
-        `tr[data-param-name="${paramName}"] input[type="text"]`
-      );
-      matchingInputs.forEach((input) => {
-        if (input !== event.target) {
-          // Exclude the input that triggered the event
-          input.value = value;
-        }
-      });
-    }
+        // Find all input fields with the same parameter name and update them
+        const matchingInputs = document.querySelectorAll(
+          `tr[data-param-name="${paramName}"] input[type="text"]`
+        );
+        matchingInputs.forEach((input) => {
+          if (input !== event.target) {
+            // Exclude the input that triggered the event
+            input.value = value;
+          }
+        });
+      }
+    });
   });
-});
 
-// Initialize a MutationObserver to watch for changes in the DOM
-const observer = new MutationObserver((mutationsList) => {
-  for (let mutation of mutationsList) {
-    if (mutation.type === "childList" || mutation.type === "attributes") {
-      const inputs = document.querySelectorAll('input[type="text"]');
-      inputs.forEach((input) => {
-        if (!input.disabled && !input.value) {
-          const paramName = input
+  document.addEventListener("keydown", function (event) {
+    chrome.storage.local.get("swechoHotkey", function (data) {
+      const hotkey = (data.swechoHotkey || "C").toUpperCase();
+      if (event.ctrlKey && event.shiftKey && event.code === `Key${hotkey}`) {
+        const activeElement = document.activeElement;
+        if (
+          activeElement.tagName === "INPUT" &&
+          activeElement.closest("tr[data-param-name]")
+        ) {
+          const paramName = activeElement
             .closest("tr[data-param-name]")
             .getAttribute("data-param-name");
           const matchingInputs = document.querySelectorAll(
             `tr[data-param-name="${paramName}"] input[type="text"]`
           );
           for (let match of matchingInputs) {
-            if (match.value) {
-              input.value = match.value;
+            if (match.value && match !== activeElement) {
+              activeElement.value = match.value;
               break;
             }
           }
         }
-      });
-    }
-  }
-});
-
-// Start observing the document with the configured parameters
-observer.observe(document.body, {
-  childList: true,
-  subtree: true,
-  attributes: true,
-  attributeFilter: ["disabled"],
-});
+      }
+    });
+  });
+})();
