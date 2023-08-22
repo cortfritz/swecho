d3fbe2cf704fe06a0904b42dadbece5d5e4e32fa
Cort Fritz <cort@rheli.co>
2023-08-22 15:14:22 -0700

    disable on non swagger and auto scan

--- content.js
+++ content.js
@@ -1,23 +1,66 @@
-document.addEventListener("input", (event) => {
-  // Check if the event target is an input field within a Swagger parameter row
-  if (
-    event.target.tagName === "INPUT" &&
-    event.target.closest("tr[data-param-name]")
-  ) {
-    const paramName = event.target
-      .closest("tr[data-param-name]")
-      .getAttribute("data-param-name");
-    const value = event.target.value;
+const isSwaggerPage = document.querySelector("body > div#swagger-ui");
 
-    // Find all input fields with the same parameter name and update them
-    const matchingInputs = document.querySelectorAll(
-      `tr[data-param-name="${paramName}"] input[type="text"]`
-    );
-    matchingInputs.forEach((input) => {
-      if (input !== event.target) {
-        // Exclude the input that triggered the event
-        input.value = value;
-      }
-    });
+if (!isSwaggerPage) {
+  // Exit the script if it's not a Swagger page
+  return;
+}
+
+document.addEventListener("input", function (event) {
+  // Fetch the 'swechoEnabled' value from Chrome storage
+  chrome.storage.local.get("swechoEnabled", function (data) {
+    if (
+      event.target.tagName === "INPUT" &&
+      event.target.closest("tr[data-param-name]") &&
+      data.swechoEnabled
+    ) {
+      const paramName = event.target
+        .closest("tr[data-param-name]")
+        .getAttribute("data-param-name");
+      const value = event.target.value;
+
+      // Find all input fields with the same parameter name and update them
+      const matchingInputs = document.querySelectorAll(
+        `tr[data-param-name="${paramName}"] input[type="text"]`
+      );
+      matchingInputs.forEach((input) => {
+        if (input !== event.target) {
+          // Exclude the input that triggered the event
+          input.value = value;
+        }
+      });
+    }
+  });
+});
+
+// Initialize a MutationObserver to watch for changes in the DOM
+const observer = new MutationObserver((mutationsList) => {
+  for (let mutation of mutationsList) {
+    if (mutation.type === "childList" || mutation.type === "attributes") {
+      const inputs = document.querySelectorAll('input[type="text"]');
+      inputs.forEach((input) => {
+        if (!input.disabled && !input.value) {
+          const paramName = input
+            .closest("tr[data-param-name]")
+            .getAttribute("data-param-name");
+          const matchingInputs = document.querySelectorAll(
+            `tr[data-param-name="${paramName}"] input[type="text"]`
+          );
+          for (let match of matchingInputs) {
+            if (match.value) {
+              input.value = match.value;
+              break;
+            }
+          }
+        }
+      });
+    }
   }
+});
+
+// Start observing the document with the configured parameters
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+  attributes: true,
+  attributeFilter: ["disabled"],
 });
